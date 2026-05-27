416ded6bc438fdbf2fc9ec6c043f5e02ea66716f
JoelFaldin <joelfaldin@gmail.com>
2026-05-26 23:49:37 -0400

    feat: add simple udp config on main

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+)
 
+func main() {
+	addr := net.UDPAddr{Port: 5454}
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	log.Printf("DNS server listening on :%d\n", 5454)
 }
