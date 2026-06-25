affb81e6802ed6d9f04cf523d49b4f12accf9c31
JoelFaldin <joelfaldin@gmail.com>
2026-06-24 23:58:00 -0400

    feat: add LoadFromFolder function in blocklist

--- internal/blocklist/blocklist.go
+++ internal/blocklist/blocklist.go
@@ -1,6 +1,8 @@
 package blocklist
 
 import (
+	"bufio"
+	"os"
 	"strings"
 	"sync"
 )
@@ -30,4 +32,50 @@ func (b *BlockStruct) IsBlocked(domain string) bool {
 
 	_, black := b.domains[domain]
 	return black
+}
+
+func (b *BlockStruct) LoadFromFolder(folderPath string) error {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.domains = make(map[string]struct{})
+
+	files, err := os.ReadDir(folderPath)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		f, err := os.Open(folderPath + "/" + file.Name())
+		if err != nil {
+			continue
+		}
+
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+
+			if line == "" || strings.HasPrefix(line, "#") {
+				continue
+			}
+
+			parts := strings.Fields(line)
+			if len(parts) > 1 {
+				line = parts[1]
+			} else {
+				line = parts[0]
+			}
+
+			domain := strings.ToLower(strings.TrimSuffix(line, "."))
+			b.domains[domain] = struct{}{}
+		}
+
+		f.Close()
+	}
+
+	return nil
 }
